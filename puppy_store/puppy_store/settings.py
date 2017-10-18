ae5de6c4616be1ac1924add87a9703c84e6aae76
Julian Toro <jtoro@tecmovin.com>
2017-10-18 15:24:08 -0500

    nuevo config

--- puppy_store/puppy_store/settings.py
+++ puppy_store/puppy_store/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'puppy_store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,6 +93,13 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
+"""
+
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
 
 
 # Password validation
